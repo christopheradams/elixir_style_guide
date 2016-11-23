56754d160a77b86eebb16e8442115f341561cbce
Christopher Adams <christopher@fabricatorz.com>
2016-11-23 17:57:29 +0800

    Proscribe inline HTML

--- markdown.rb
+++ markdown.rb
@@ -3,5 +3,4 @@ all
 rule 'MD007', :indent => 4 # Unordered list indentation
 
 exclude_rule 'MD012' # Multiple consecutive blank lines
-exclude_rule 'MD033' # Inline HTML
 exclude_rule 'MD036' # Emphasis used instead of a header
