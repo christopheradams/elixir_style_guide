4fa500bac695969b4d079ccfce659caf814a9e97
Christopher Adams <christopher@fabricatorz.com>
2016-11-29 13:55:26 +0800

    Add a mix file

---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule ElixirStyleGuide.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :elixir_style_guide,
+     version: "0.1.0",
+     elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+end
