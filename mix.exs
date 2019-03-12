fedefb2a9f042789e20a04125947d882a46e074e
Christopher Adams <christopher@fabricatorz.com>
2019-03-12 17:53:20 +0800

    Format Mixfile

--- mix.exs
+++ mix.exs
@@ -9,16 +9,18 @@ defmodule ElixirStyleGuide.Mixfile do
   @source_url "https://github.com/christopheradams/elixir_style_guide"
 
   def project do
-    [app: :elixir_style_guide,
-     version: @version,
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     docs: docs(),
-     description: @project_description,
-     source_url: @source_url,
-     package: package(),
-     deps: deps()]
+    [
+      app: :elixir_style_guide,
+      version: @version,
+      elixir: "~> 1.0",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      docs: docs(),
+      description: @project_description,
+      source_url: @source_url,
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
